e538402164995a828c16f7ef77d866421a374318
afrolino02 <andres.puello.gomez@gmail.com>
2021-08-18 12:54:10 -0500

    ah pues

---
+++ nueva seccion/script.js
@@ -0,0 +1,9 @@
+
+ScrollReveal().reveal('headline');
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', {
+	delay: 5000,
+});
+ScrollReveal().reveal('.punchline', {
+	delay: 5000
+});
