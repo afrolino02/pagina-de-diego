fe331cfd7dac9b465dfbc03bf8aaccb4164c990f
afrolino02 <andres.puello.gomez@gmail.com>
2021-08-23 10:55:18 -0500

    Carga 23/08/21

--- nueva seccion/script.js
+++
@@ -1,9 +0,0 @@
-
-ScrollReveal().reveal('headline');
-ScrollReveal().reveal('.headline');
-ScrollReveal().reveal('.tagline', {
-	delay: 5000,
-});
-ScrollReveal().reveal('.punchline', {
-	delay: 5000
-});
